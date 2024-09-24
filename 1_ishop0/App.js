98e62f675c1789466e7be0e9332d5749b61447c6
Nikolai <114181254+MrDenza@users.noreply.github.com>
2024-09-24 22:56:36 +0300

    Add homework 1_ishop0

---
+++ 1_ishop0/App.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TitleShop from './components/TitleShop';
+
+ReactDOM.render(
+  <TitleShop question={"Интернет-магазин \"НотеБук\""}/>, 
+  document.getElementById('container') 
+);
