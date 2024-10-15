ade9c437b3c7f855444d6b73378fe9c6ec41e84f
Nikolai <114181254+MrDenza@users.noreply.github.com>
2024-10-15 20:45:48 +0300

    Add homework 6_RainbowFrame

---
+++ 6_RainbowFrame/source/App.js
@@ -0,0 +1,33 @@
+﻿import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+
+let colors = [
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    '#00BFFF',
+    'blue',
+    'purple',
+    // '#8b8b8b', 
+    // '#7f1594',
+    // '#09ff00', 
+    // 'orange', 
+    // '#003cff', 
+    // '#ff0000',
+    // 'darksalmon',
+    // 'yellow',
+];
+
+import RainbowFrame from "./components/RainbowFrame";
+
+ReactDOM.render(
+    <Fragment>
+        <div className = "body__container">
+            <RainbowFrame colors = {colors}>Hello, world!</RainbowFrame>
+        </div>
+    </Fragment>,
+    document.getElementById('container') 
+);
---
+++ 6_RainbowFrame/source/components/RainbowFrame.js
@@ -0,0 +1,22 @@
+﻿import React, {Fragment} from "react";
+
+import "./RainbowFrame.css";
+
+class RainbowFrame extends React.Component {
+
+    render() {
+        let RainbowElem = this.props.colors.reduce( (acc, c, i) => {
+            return (
+                <div key={i} style={{border: `5px solid ${c}`, padding: "10px"}}>{acc}</div>
+            );   
+        }, this.props.children);
+
+        return (
+            <Fragment>
+                {RainbowElem}
+            </Fragment>
+        );
+    }
+}
+
+export default RainbowFrame;
