e3420de43651f79698b9aa38ecc87cdebdc10ee7
Nikolai <114181254+MrDenza@users.noreply.github.com>
2024-10-15 22:56:10 +0300

    Add homework 7_br2jsx

---
+++ 7_br2jsx/source/App.js
@@ -0,0 +1,15 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+
+let text = "первый<br>второй<br/>третий<br />последний";
+
+import BR2JSX from "./components/BR2JSX";
+
+ReactDOM.render(
+    <div className = "body__container">
+        <BR2JSX text = {text}/>
+    </div>
+    ,document.getElementById('container') 
+);
---
+++ 7_br2jsx/source/components/BR2JSX.js
@@ -0,0 +1,30 @@
+﻿import React from "react";
+
+import "./BR2JSX.css";
+
+class BR2JSX extends React.Component {
+
+    render() {
+        let textAndBr = Array.from(this.props.text.split(/[<br><br/><br />]+/).reduce( (accText, elem, i) => {
+                (accText.length === 0) ? accText.push(elem) : accText.push(<br key={i}/>, elem);
+                return accText;
+
+                // Ниже способ дороже по производительности? 
+                // Каждый раз создание нового массива, деструктуризация,
+                //    добавление в новый массив и с каждым разом объёмнее процедуры.
+                // if (accText.length === 0) {
+                //     return [elem];
+                // }
+                //return [...accText, <br key={i}/>, elem]; 
+            }, [])
+        )
+
+        return (
+            <div className="br2jsx">
+                {textAndBr}
+            </div>       
+        );
+    }
+}
+
+export default BR2JSX;
