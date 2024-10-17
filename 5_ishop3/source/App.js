c795edec22a1e8bc6c39c305644d71d18cbd38f5
Nikolai <114181254+MrDenza@users.noreply.github.com>
2024-10-17 21:25:26 +0300

    Add homework 5_ishop3

---
+++ 5_ishop3/source/App.js
@@ -0,0 +1,19 @@
+﻿import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import "./style.css";
+
+let titleShop = "Интернет-магазин \"НотеБук\"";
+let addressShop = "Беларусь, г. Минск, просп. Победителей, д. 9";
+import productDB from "../public/productDatabase.json";
+
+import Shop from "./components/Shop";
+
+ReactDOM.render(
+    <Fragment>
+        <div className = "body__container">
+            <Shop title = {titleShop} address = {addressShop} db = {productDB}/>
+        </div>
+    </Fragment>,
+    document.getElementById('container') 
+);
---
+++ 5_ishop3/source/components/ProductPos.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import "./ProductPos.css"
+
+class ProductPos extends React.Component {
+
+    enterPos = (eo) => {
+        this.props.cbEnterPos(this.props.code);
+    }
+    
+    enterDeletePos = (eo) => {
+        eo.stopPropagation();
+        this.props.cbDeletePos(this.props.code);
+    }
+    
+    render() {
+        return (
+           <li className={`product-list__pos ${(this.props.selectPos === this.props.code) && "product-list__pos_select"}`} onClick={this.enterPos}>
+                <span className="product-list__pos-title">{this.props.title}</span>
+                <div className="product-list__pos-box-photo">
+                    <img className="product-list__pos-photo" src={this.props.photo} alt={`Внешний вид ${this.props.title}`}></img>
+                </div>
+                <span className="product-list__pos-price">{`${this.props.sum} BYN`}</span>
+                <span className="product-list__pos-lot">{`Остаток на складе: ${this.props.lot} шт.`}</span>
+                <span className="product-list__pos-subtitle">Характеристики:</span>
+                <span className="product-list__pos-text">{this.props.text}</span>
+                <div className="product-list__pos-box-btn">
+                    <label htmlFor="btn-del" className="product-list__pos-box-btn-del">
+                        <input type="button" id="btn-del" className="product-list__pos-btn-del" value="Удалить" onClick={this.enterDeletePos}></input>
+                    </label>
+                    <label htmlFor="btn-edit" className="product-list__pos-box-btn-edit">
+                        <input type="button" id="btn-edit" className="product-list__pos-btn-edit" value="Редактировать" onClick={this.enterDeletePos}></input>
+                    </label>
+                </div>
+            </li>
+        );
+    }
+}
+
+export default ProductPos;
